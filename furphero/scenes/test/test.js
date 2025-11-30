36a52c9fa34fd2768e6233839be552bd61809ad1
Sean Collins <bigbadseansean@gmail.com>
2025-11-30 16:37:48 -0700

    Add furp dance and evil darkness

--- furphero/scenes/test/test.js
+++ furphero/scenes/test/test.js
@@ -1,107 +1,174 @@
 class TestScene extends Scene {
-  // Variables for scene
-  cubes = [];
-  lights = [];
-  time = 0.0;
-  pvm = null;
-  normalMat = null;
-  cameraPos = null;
-  textureSampler = null;
 
-  constructor() {
-    super();
-  }
+    // Variables for scene
+    cubes = [];
+    time = 0.0;
+    pvm = null; 
+    cameraPos = null;
+    textureSampler = null;
 
-  async init(state) {
-    // Load scene data from json
-    await this.load(state, "furphero/scenes/test/test.json");
+    action = "Stay";
+    jumptime = 0.0;
+    prevAction = "Stay";
 
-    state.camera.rotateY(-180.0, 3.0);
-    state.camera.position[1] = 3.0;
-    // state.camera.position[2] = -4.0;
-    state.camera.update();
+    constructor() {
+        super()
 
-    // Aquire nodes so we don't hash on every update
-    // Get existing cube
-    let cube0 = this.get("cube");
-    this.lights = this.get("lights");
-    // mat4.scale(cube0.transform, cube0.transform, [0.9,0.9,0.9]);
-    // mat4.translate(cube0.transform, cube0.transform, [0.0,0.0,0.0]);
+        this.action = "Stay";
+        this.prevAction = "Stay";
+        this.jumptime = 0.0;
 
-    // Duplicate cube node into multiple instances
-    let spread = 1.0;
-    let base_spread = 3.5;
-    let crowd_density = 30;
-    let crowd_size = 10;
-    for (let j = 0; j < crowd_size; j++) {
-      for (let i = 0; i < crowd_density; i++) {
-        let curl = Math.random();
-        let rad = (((i * 0.7 + curl) / 10.0) * 360.0 * Math.PI) / 180.0;
-        let newcube = this.cloneAs("cube" + i + ":" + j, cube0);
-        if (newcube != null) {
-          newcube.transform = mat4.clone(newcube.transform);
-          mat4.scale(newcube.transform, newcube.transform, [0.5, 0.5, 0.5]);
-          mat4.translate(newcube.transform, newcube.transform, [
-            Math.cos(rad) * (base_spread + spread * j),
-            -0.5,
-            Math.sin(rad) * (base_spread + spread * j),
-          ]);
-          mat4.rotateY(
-            newcube.transform,
-            newcube.transform,
-            -(rad + (Math.PI * 90) / 180),
-          );
-        }
-        this.cubes.push(newcube);
-      }
+        window.addEventListener("keydown", (e) => {
+            if(e.code === "ArrowUp") {
+                this.action = "Shake";
+            }
+            if(e.code === "ArrowDown") {
+                this.action = "Spin";
+            }
+            if(e.code === "ArrowLeft") {
+                this.action = "Jump";
+                this.jumptime = this.time;
+            }
+            if(e.code === "ArrowRight") {
+                this.action = "Wave";
+            }
+            if(e.code === "Space") {
+                this.action = "Stay";
+            }
+        });
     }
-    this.cubes.push(cube0);
 
-    // Aquire uniform objects from the GPUState so we can update them
-    this.pvm = state.getUniform("simpletexture", "pvm");
-    this.pvm.set(mat4.create());
-    this.normalMat = state.getUniform("simpletexture", "normalMat");
-    this.normalMat.set(mat4.create());
+    async init(state) {
+        // Load scene data from json
+        await this.load(state, "furphero/scenes/test/test.json")
 
-    this.cameraPos = state.getUniform("simpletexture", "uCameraPos");
-    this.textureSampler = state.getUniform("simpletexture", "uTexture");
-    this.textureSampler.set(0);
-    this.textureSampler.update();
-  }
+        state.camera.rotateY(-180.0,3.0);
+        state.camera.position[1] = 3.0;
+        // state.camera.position[2] = -4.0;
+        state.camera.update();
 
-  update(delta) {
-    this.time += delta;
+        // Aquire nodes so we don't hash on every update
+        // Get existing cube 
+        let cube0 = this.get("cube");
+        cube0.baseTransform = mat4.clone(cube0.transform);
+        // mat4.scale(cube0.transform, cube0.transform, [0.9,0.9,0.9]);
+        // mat4.translate(cube0.transform, cube0.transform, [0.0,0.0,0.0]);
 
-    state.camera.rotateY((this.time / 7000.0) * 360.0, 2.0);
-    this.cameraPos.set(state.camera.position);
-    state.camera.update();
+        // Duplicate cube node into multiple instances
+        let spread = 1.0;
+        let base_spread = 3.5;
+        let crowd_density = 30;
+        let crowd_size = 10;
+        for(let j=0; j<crowd_size; j++) {
+            for(let i=0; i<crowd_density; i++) {
+                let curl = Math.random();
+                let rad = (((((i*0.7)+curl)/10.0)*360.0)*Math.PI)/180.0
+                let newcube = this.cloneAs("cube"+i+":"+j, cube0);
+                if(newcube != null) {
+                    newcube.transform = mat4.clone(newcube.transform);
+                    mat4.scale(newcube.transform, newcube.transform, [0.5, 0.5, 0.5]);
+                    mat4.translate(newcube.transform, newcube.transform, [Math.cos(rad)*(base_spread+(spread*j)), -0.5, Math.sin(rad)*(base_spread+(spread*j))]);
+                    mat4.rotateY(newcube.transform, newcube.transform, -(rad+(Math.PI*90/180)));
+                    newcube.baseTransform = mat4.clone(newcube.transform);
+                    this.cubes.push(newcube);
+                }
+                
+            }
+        }
+        this.cubes.push(cube0);
 
-    // Duplicate cube node into multiple instances
+        // Aquire uniform objects from the GPUState so we can update them
+        this.pvm = state.getUniform("simpletexture", "pvm");
+        this.pvm.set(mat4.create());
 
-    return FLOW.RENDER;
-  }
+        this.normalMat = state.getUniform("simpletexture", "normalMat");
+        this.normalMat.set(mat4.create());
 
-  render(state) {
-    // Here any pre-rendering or scene-global uniforms can be updated
+        this.cameraPos = state.getUniform("simpletexture", "uCameraPos");
+        this.textureSampler = state.getUniform("simpletexture", "uTexture");
+        this.textureSampler.set(0);        
+        this.textureSampler.update();
 
-    this.nodes.forEach((node) => {
-      // Set and update node uniforms to GPU
-      // Here specifically we're using the GPUState's camera to make a pvm value
-      state.camera.calcPVM(this.pvm.value, node.transform);
-      this.pvm.update();
+    }
 
-      let normalMat = mat4.create();
-      mat4.invert(normalMat, node.transform);
-      mat4.transpose(normalMat, normalMat);
+    update(delta) {
 
-      this.normalMat.set(normalMat);
-      this.normalMat.update();
-      // console.log(normalMat);
+        this.time += delta;
+        if(this.action !== this.prevAction) {
+            this.cubes.forEach((cube) =>{
+                if (!cube || !cube.baseTransform) return;
+                mat4.copy(cube.transform, cube.baseTransform)
+            });
+            if (this.action === "Jump") {
+                this.jumptime = this.time;
+            }
+            this.prevAction = this.action;
+        }
+        
+        this.cubes.forEach((cube) =>{
+   
+            // Shake what yo mama gave ya
+            if (this.action === "Shake") {
+                 mat4.rotateY(cube.transform, cube.transform, Math.sin(this.time/100.0)*0.01*(360*Math.PI/180.0));
+            }
+            // Spin me right round baby right round
+            if (this.action === "Spin") {
+                mat4.rotateY(cube.transform, cube.transform, 0.02*(360*Math.PI/180.0));
+            }
+            // Jump up jump up and get down
+            if (this.action === "Jump") {
+                mat4.copy(cube.transform, cube.baseTransform);
 
-      this.cameraPos.update();
+                const elaped = this.time-this.jumptime;
+                const period = 700.0;
 
-      // Render nodes
-      node.render();
-    });
-  }
+                const phase = (elaped /period) *2.0 * Math.PI;
+                const bounce = (Math.sin(phase) + 1.0) *0.5 * 0.6;
+                mat4.translate(cube.transform, cube.transform, [0.0, bounce, 0.0]);
+            }
+            // Wave back and forth
+            if (this.action === "Wave") {
+                mat4.copy(cube.transform, cube.baseTransform);
+
+                const tilt = Math.sin(this.time / 200.0) * (10 * Math.PI / 180.0); 
+
+                mat4.rotateZ(cube.transform, cube.transform, tilt);            }
+            // Stop collaborate and listen
+            if (this.action === "Stay") {   
+                    mat4.copy(cube.transform, cube.baseTransform)
+            }
+        
+        });
+        state.camera.rotateY((this.time/7000.0)*360.0,2.0);
+        state.camera.update();
+
+        this.cameraPos.set(state.camera.position);
+
+        return FLOW.RENDER;
+    }
+
+    render(state) {
+
+        // Here any pre-rendering or scene-global uniforms can be updated
+
+        this.nodes.forEach(node => {
+
+            // Set and update node uniforms to GPU 
+            // Here specifically we're using the GPUState's camera to make a pvm value
+            state.camera.calcPVM(this.pvm.value, node.transform);
+            this.pvm.update();
+            this.cameraPos.update();
+
+            let normalMat = mat4.create();
+            mat4.invert(normalMat, node.transform);
+            mat4.transpose(normalMat, normalMat);
+
+            this.normalMat.set(normalMat);
+            this.normalMat.update();
+
+            // Render nodes 
+            node.render();
+        });
+    }
+
 }
