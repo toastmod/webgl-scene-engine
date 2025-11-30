a9102294a6094d90840b32a91a62d687d1a57a1e
Isaac McCracken <isaac.mccracken@gmail.com>
2025-11-30 15:06:36 -0700

    we need the vertex normals

--- furphero/shaders/simpletexture/fragment.glsl
+++ furphero/shaders/simpletexture/fragment.glsl
@@ -2,18 +2,28 @@
 precision highp float;
 
 in vec2 oUV;
+in vec3 oNormal;
 in vec3 oFragPos;
 
 out vec4 fragColor;
+
 uniform vec3 uCameraPos;
 uniform mat4 pvm;
 uniform sampler2D uTexture;
 
 void main() {
-    vec3 diffuseColor = texture(uTexture, vec2(oUV.x, abs(oUV.y-1.0))).rbg;
-    vec3 V = normalize(uCameraPos - oFragPos);
+    // Corrected texture sampling
+    vec3 diffuseColor = texture(uTexture, vec2(oUV.x, abs(oUV.y - 1.0))).rgb;
+
+    // Normalize vectors
     vec3 N = normalize(oNormal);
-    vec3 L = normalize(vec3(10, 10, 10) - oFragPos);
-    vec3 diffuse  = max(0.0, dot(N, L) * diffuseColor;
-    fragColor = vec3(diffuse, 1.0);
+    vec3 V = normalize(uCameraPos - oFragPos);
+    vec3 L = normalize(vec3(10.0, 10.0, 10.0) - oFragPos);
+
+    // Lambert diffuse
+    float diff = max(dot(N, L), 0.0);
+    vec3 diffuse = diff * diffuseColor;
+
+    // Assign a vec4 to fragColor (RGBA)
+    fragColor = vec4(diffuse, 1.0);
 }
--- furphero/shaders/simpletexture/vertex.glsl
+++ furphero/shaders/simpletexture/vertex.glsl
@@ -1,15 +1,20 @@
 #version 300 es
 precision highp float;
 
-uniform mat4 pvm;
+in vec3 aNormal;
 in vec3 aPosition;
 in vec2 aUV;
+
+out vec3 oNormal;
 out vec2 oUV;
 out vec3 oFragPos;
 
+uniform mat4 pvm;
+uniform mat4 normalMat;
 
 void main() {
     oUV = aUV;
-    oFragPos = pvm * vec4(aPosition, 1.0);
-    gl_Position = oFragPos;
+    oNormal = (normalMat * vec4(aNormal, 0.0)).xyz;
+    gl_Position = pvm * vec4(aPosition, 1.0);
+    oFragPos = gl_Position.xyz;
 }
