9f5c08cecfdafe608f2d4009b605d002f385dfc8
Andrew Numrich <toastymod@gmail.com>
2025-12-14 13:03:22 -0700

    Added color shader

---
+++ furphero/shaders/simplecolor/fragment.glsl
@@ -0,0 +1,35 @@
+#version 300 es
+precision highp float;
+
+in vec3 oNormal;
+in vec3 oFragPos;
+
+out vec4 fragColor;
+
+uniform vec3 uCameraPos;
+uniform mat4 pvm;
+uniform vec3 uColor;
+
+uniform float uMaxFade; 
+uniform float uMinFade; 
+
+void main() {
+    // Corrected texture sampling
+    vec3 diffuseColor = uColor;
+
+    // Distance fade
+    float alpha = uMinFade - oFragPos.z / (uMaxFade - uMinFade); 
+
+    // Normalize vectors
+    vec3 N = normalize(oNormal);
+
+    vec3 V = normalize(uCameraPos - oFragPos);
+    vec3 L = normalize(vec3(0.0, 6.0, 6.0) - oFragPos);
+    //vec3 H = Normalize(V +
+    // Lambert diffuse
+    float diff = max(dot(N, L), 0.0);
+    vec3 diffuse = diff * diffuseColor + vec3(0.05, 0.05, 0.1);
+
+    // Assign a vec4 to fragColor (RGBA)
+    fragColor = vec4(diffuse, alpha);
+}
